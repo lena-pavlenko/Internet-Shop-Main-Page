bbfe2407bb6eba3a7261fdfef0c109c339c9d827
Lena Pavlenko <pawlencko.alena2010@yandex.ru>
2022-11-17 22:40:19 +0300

    create scss, header

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const imagemin = require('gulp-imagemin');
-// const include = require('gulp-file-include');
 
 const paths = {
     html: {
@@ -14,16 +13,20 @@ const paths = {
         dest: 'dist/'
     },
     styles: {
-        src: 'src/styles/**/*.scss',
+        src: 'src/css/**/style.scss',
         dest: 'dist/css/'
     },
     scripts: {
-        src: 'src/scripts/**/*.js',
+        src: 'src/js/**/*.js',
         dest: 'dist/js/'
     },
     images: {
         src: 'src/img/*',
         dest: 'dist/img'
+    },
+    fonts: {
+        src: 'src/fonts/*',
+        dest: 'dist/fonts'
     }
 }
 
@@ -32,14 +35,20 @@ function html() {
     .pipe(gulp.dest(paths.html.dest));
 }
 
+function fonts() {
+    return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
+}
+
 function styles() {
     return gulp.src(paths.styles.src)
-    .pipe(sourcemaps.init())
+    
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(sourcemaps.write('.'))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
@@ -68,9 +77,10 @@ function watch() {
     gulp.watch(paths.scripts.src, scripts)
     gulp.watch(paths.images.src, img)
     gulp.watch(paths.html.src, html)
+    gulp.watch(paths.fonts.src, fonts)
 }
 
-const build = gulp.series(clean, gulp.parallel(html, styles, scripts, img), watch);
+const build = gulp.series(clean, gulp.parallel(html, styles, scripts, img, fonts), watch);
 
 function clean() {
     return del(['dist'])
@@ -81,6 +91,7 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.img = img;
 exports.html = html;
+exports.fonts = fonts;
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
